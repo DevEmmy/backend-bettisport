bfcac85c618120dc14a807abfce2f84e1c8b16e3
web-tranquility <eolaosebikan60@gmail.com>
2024-07-23 07:48:56 +0100

    added notification model

---
+++ src/models/notification.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface INotification extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  title: string;
+  message: string;
+  read: boolean;
+}
+
+const notificationSchema: Schema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  message: { type: String, required: true },
+  read: { type: Boolean, default: false },
+}, { timestamps: true });
+
+const Notification = mongoose.model<INotification>('Notification', notificationSchema);
+
+export default Notification;
